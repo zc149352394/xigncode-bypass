239cf57eac205418d3cc20572f7aa8d2c59fb408
海比丶天蓝 <149352394@qq.com>
2019-08-18 02:31:27 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Xigncode-bypass
-This mod requires latest version of toolbox! it can be installed just like any other mod(๑•̀ㅂ•́)و✧
+# xigncode-bypass
+
+适用于 TeraToolbox 的版本
+
+# 安装说明
+
+- 请删除客户端 \TERA\Binaries\XIGNCODE 中"垃圾文件"避免自动上传, 仅需保留以下4个文件即可
+
+  [x3.xem] [xcorona.xem] [xcorona_x64.xem] [xnina.xem]
+
+- 将整个文件夹 存放到目录 \teraProxy\mod\
+
+![screenshot](https://github.com/zc149352394/xigncode-bypass/blob/master/screenshot/01.png)
+
+![screenshot](https://github.com/zc149352394/xigncode-bypass/blob/master/screenshot/02.png)
+
+![screenshot](https://github.com/zc149352394/xigncode-bypass/blob/master/screenshot/03.png)
+
+![screenshot](https://github.com/zc149352394/xigncode-bypass/blob/master/screenshot/04.png)
+
+!! 等待Tera客户端后台进程完全结束, 并出现如下提示(恢复这2个文件)
+
+![screenshot](https://github.com/zc149352394/xigncode-bypass/blob/master/screenshot/05.png)
+
+# 更新支持版本:
+
+- Node.js 11.4.0 到 11.15.0 (NODE_MODULE_VERSION: x32_67 x64_67)
+
+- Node.js 12.0.0 到 12.8.1 (NODE_MODULE_VERSION: x32_72 x64_72)
+
+- Electron v4.0 到 4.2.9 (NODE_MODULE_VERSION: x32_64 x64_64)
+
+- Electron v5.0 到 5.0.9 (NODE_MODULE_VERSION: x32_70 x64_70)
+
--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 const { execFileSync } = require('child_process');
+const ProcessListener = require("./process-listener");
 
 module.exports = function XigncodeBypass(mod) {
     if (['eu', 'na', 'ru'].includes(mod.region)) {
         mod.warn(`Not required for region ${mod.region.toUpperCase()}!`);
         return;
     }
-
+    
+    let PatchedProcesses = {};
+    
+    function HandleAddedProcess(process) {
+        try {
+            let XigncodeFolder = path.join(path.dirname(process.path), "XIGNCODE");
+            PatchedProcesses[process.pid] = XigncodeFolder;
+            // console.log(`[bypass] Game client (PID ${process.pid}) detected, bypass installed.`);
+        } catch(e) {
+            // Ignore errors...
+        }
+    }
+    
+    function HandleRemovedProcess(pid) {
+        try {
+            let XigncodeFolder = PatchedProcesses[pid];
+                fs.copyFileSync(path.join(__dirname, "bak/x3.xem"), path.join(XigncodeFolder, "x3.xem"));
+                fs.copyFileSync(path.join(__dirname, "bak/xcorona.xem"), path.join(XigncodeFolder, "xcorona.xem"));
+            delete PatchedProcesses[pid];
+            console.log(`[bypass] Game client (PID ${pid}) closed, bypass reverted.`);
+        } catch(e) {
+            // Ignore errors...
+        }
+    }
     // Remove vulnerable driver from system
     try {
         fs.unlinkSync('C:\\Windows\\xhunter1.sys');
@@ -17,10 +41,13 @@ module.exports = function XigncodeBypass(mod) {
     } catch(e) {
         // Ignore errors...
     }
-
+    
     // Inject bypass DLL
     try {
         execFileSync(path.join(path.dirname(require.resolve('tera-client-interface')), 'injector.exe'), [mod.clientInterface.info.pid, path.join(__dirname, 'xigncode-bypass.dll')]);
+        
+        ProcessListener("TERA.exe", HandleAddedProcess, HandleRemovedProcess, 500);
+        // console.log("[bypass] Ready, waiting for game client to be started!");
     } catch(e) {
         mod.error(`Unable to install bypass (PID ${mod.clientInterface.info.pid})!`);
         switch (e.code) {
@@ -36,15 +63,13 @@ module.exports = function XigncodeBypass(mod) {
             }
             default: {
                 switch (e.status) {
-                    case 1:
-                    {
+                    case 1: {
                         mod.error('Bypass DLL injection unsuccessful. It has likely been blocked by your anti-virus.');
                         mod.error('> Make sure that TERA Toolbox is running with Administrator privileges!');
                         mod.error('> Disable/uninstall your anti-virus or whitelist TERA Toolbox and injector.exe!');
                         break;
                     }
-                    default:
-                    {
+                    default: {
                         mod.error('This is likely caused by your anti-virus interfering. Disable/uninstall it or whitelist TERA Toolbox.');
                         mod.error('Full error message:');
                         mod.error(e);
